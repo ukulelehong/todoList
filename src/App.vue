242b2e54f8941b95e60f67f52f8a76529b5181e8
ukulelehong <1052940613@qq.com>
2022-12-12 17:51:35 +0800

    feat:add item and items

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,12 @@ import ItemInput from "./components/ItemInput.vue";
   <div class="bg-red-50 h-screen flex flex-col items-center">
     <Header />
     <ItemInput />
+    <ul>
+      <li class="flex w-96 h-14 bg-black">
+        <div class=" bg-red-50 ">1</div>
+        <div class=" bg-green-50">2</div>
+        <div class=" bg-blue-50">3</div>
+      </li>
+    </ul>
   </div>
 </template>
