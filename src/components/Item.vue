1eba8903714a99cf65d9896b877179e5dcaa190f
ukulelehong <1052940613@qq.com>
2023-01-12 17:52:37 +0800

    feat: 拆分组件（后面删除）

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+    <li class="todolist flex w-102 h-14 bg-white border">
+        <div v-if="true" class=" toggle w-12"></div>
+        <div v-else class=" toggle-checked w-12"></div>
+        <div class="flex-grow flex items-center p-4"><span>12346</span></div>
+        <div class="w-12 flex justify-center items-center"><button class="text-3xl">×</button></div>
+    </li>
+</template>
